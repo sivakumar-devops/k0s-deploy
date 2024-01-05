1bd82e1f330ccf2727ca8bf8ac5d2e8397c954bf
sivakumar-devops <92422557+sivakumar-devops@users.noreply.github.com>
2024-01-05 16:10:14 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,23 +3,24 @@
 
 set -e
 
-# Variable declaration.
-directory="/mnt/boldbi_data"
+# variable declaration
+repo_url="https://github.com/sivakumar-devops/k0s-deploy/raw/main/private-cloud.zip"
+destination="/manifest"
 
 # Parse command-line arguments
 for arg in "$@"; do
   case $arg in
-    --aws-access-key=*)
-      aws_access_key="${arg#*=}"
+    --storage-account-name=*)
+      storage_account_name="${arg#*=}"
       ;;
-    --aws-access-secret=*)
-      aws_secret_access_key="${arg#*=}"
+    --storage-account-key=*)
+      storage_account_key="${arg#*=}"
       ;;
-    --s3-bucket=*)
-      s3_bucket="${arg#*=}"
+    --fileshare-name=*)
+      fileshare_name="${arg#*=}"
       ;;
-    --domain=*)
-      domain="${arg#*=}"
+    --app_base_url=*)
+      app_base_url="${arg#*=}"
       ;;
   esac
 done
@@ -54,22 +55,20 @@ function install_zip {
   fi
 }
 
-# Function to install s3fs if not present
-function install_s3fs {
-  if ! command_exists s3fs; then
+# Function to install nginx if not present
+function install_nginx {
+  if ! command_exists nginx; then
     say 4 "Installing s3fs..."
     sudo apt-get update
-    sudo apt-get install -y s3fs
-    say 2 "s3fs installed successfully."
+    sudo apt-get install -y nginx
+    say 2 "nginx installed successfully."
   else
-    say 2 "s3fs is already installed."
+    say 2 "nginx is already installed."
   fi
 }
 
 # Function to download and unzip GitHub repository
-function download_and_unzip {
-  repo_url=$1
-  destination=$2
+function download_and_unzip_manifest {
   [ -d "$destination" ] && rm -r "$destination" ; mkdir "$destination" || mkdir "$destination"
   say 4 "Downloading and extracting GitHub repository..."
   curl -sSL $repo_url -o repo.zip
@@ -77,41 +76,86 @@ function download_and_unzip {
   rm repo.zip
 }
 
-# Function to create a new directory
-function create_directory {
-  directory=$1
-  say 4 "Creating directory: $directory"
-  sudo mkdir -p $directory
+# Function to mount fileshare
+function update_fileshare_name {
+  pvconfig_file="$destination/private-cloud/boldbi/configuration/pvclaim_azure_smb.yaml"
+  if [-f "$pvconfig_file"]; then
+    sed -i -e "s/^ *shareName: <fileshare>/  shareName: $fileshare_name/" "$pvconfig_file"
+  else
+    handle_error "Pvcalim file is not availble"
+  fi 
+  kustomfile="$destination/private-cloud/kustomization.yaml"
+  set -i -e "s/^ *#- boldbi/configuration/pvclaim_azure_smb.yaml/  - boldbi/configuration/pvclaim_azure_smb.yaml/" "$kustomfile"
+  set -i -e "s/^ *- boldbi/configuration/pvclaim_onpremise.yaml/   #- boldbi/configuration/pvclaim_onpremise.yaml/" "$kustomfile"
 }
 
-function domain_mapping  {
-# File path to your YAML configuration file
-config_file="/manifest/private-cloud/boldbi/ingress.yaml"
-
-# Domain to replace with
-new_domain="$domain"
-
-# Uncomment and replace domain in the specified lines
-sed -i -e "s/^ *#tls:/  tls:/" "$config_file"
-sed -i -e "s/^ *#- hosts:/  - hosts:/" "$config_file"
-sed -i -e "s/^ *#- example.com/    - $new_domain/" "$config_file"
-sed -i -e "s/^ *#secretName: bold-tls/    secretName: bold-tls/" "$config_file"
-sed -i -e "s/^ *- #host: example.com/  - host: $new_domain/" "$config_file"
+app_base_url_mapping {
+  deploy_file="$destination/private-cloud/boldbi/deployment.yaml"
+  set -i -e "s/^ *value: <application_base_url>/  value: $app_base_url/" "$kustomfile"
 }
 
-# Function to mount S3 bucket
-function mount_s3_bucket {
-  directory=$1
-  aws_access_key=$2
-  aws_secret_access_key=$3
-  s3_bucket=$4
+nginx_configuration {
+  cluster_ip=$(kubectl get service ingress-nginx-controller -n ingress-nginx -o jsonpath='{.spec.clusterIP}')
+  domain=$(echo "$app_base_url" | sed 's~^https\?://~~')
+  nginx_conf="/etc/nginx/sites-available/default"
+  # Check if the file exists
+  if [ -e "$nginx_conf" ]; then
+      # If the file exists, remove it
+      rm "$nginx_conf"
+  fi
 
-  say 4 "Mounting S3 bucket..."
-  echo "$aws_access_key:$aws_secret_access_key" > ~/.passwd-s3fs
-  chmod 600 ~/.passwd-s3fs
-  s3fs $s3_bucket $directory -o passwd_file=~/.passwd-s3fs -o url=https://s3.amazonaws.com
-  sudo mount -a
-  say 2 "S3 bucket mounted successfully."
+  if [ -n "$app_base_url" ]; then
+  echo "
+    server {
+    listen 80;
+    server_name $domain;
+    return 301 https://$domain$request_uri;
+    }
+    server {
+            server_name   #domain;
+            
+            listen 443 ssl;
+            ssl_certificate /etc/nginx/sites-available/domain.pem;
+            ssl_certificate_key /etc/nginx/sites-available/domain.key;
+    location / {
+            proxy_pass http://$cluster_ip;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_http_version 1.1;
+            proxy_set_header   Upgrade $http_upgrade;
+            proxy_set_header   Connection keep-alive;
+            proxy_set_header   Host $http_host;
+            proxy_cache_bypass $http_upgrade;
+            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header   X-Forwarded-Proto $http_x_forwarded_proto;
+            proxy_set_header X-Forwarded-Host $host;
+        }
+    }" | sudo tee "$nginx_conf"
+  else
+    # Create the default nginx configuration
+    echo "
+      server {
+          listen 80 default_server;
+          listen [::]:80 default_server;
+      
+          location / {
+              proxy_pass http://$cluster_ip;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_http_version 1.1;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection keep-alive;
+              proxy_set_header Host $http_host;
+              proxy_cache_bypass $http_upgrade;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $http_x_forwarded_proto;
+              proxy_set_header X-Forwarded-Host $host;
+          }
+      }" | sudo tee "$nginx_conf"
+    fi
+
+    # Provide execution permission to the file
+    sudo chmod +x "$nginx_conf"
+    nginx -t
+    nginx -s reload
 }
 
 function show_bold_bi_graphic {
@@ -147,32 +191,31 @@ function start_k0s {
 }
 
 function install_boldbi {
-  if [ -n "$aws_access_key" ] && [ -n "$aws_secret_access_key" ] && [ -n "$s3_bucket" ]; then
-    install_s3fs
-    create_directory $directory
-    mount_s3_bucket $directory "$aws_access_key" "$aws_secret_access_key" "$s3_bucket"
-  else
-    say 3 "Skipping S3 bucket mounting details are not provided."
-  fi
+  install_nginx
   install_zip
-
+  download_and_unzip_manifest
   Install_k0s
-
   start_k0s
 
+  say 4 "Checking app_base_url provided"
+  if [ -n "$app_base_url" ]; then
+    app_base_url_mapping
+  else
+    say 3 "Skipping app_base_url mapping as it is not provided"
+  fi
+
+  
+  
   if ! k0s kubectl get nodes &> /dev/null; then
     handle_error "k0s cluster is not running."
   fi
-
-  repo_url="https://github.com/sivakumar-devops/k0s-deploy/raw/main/private-cloud.zip"
-  destination="/manifest"
-  download_and_unzip $repo_url $destination
-
-  say 4 "Checking domain provided"
-  if [ -n "$domain" ]; then
-    domain_mapping
+  
+  if [ -n "$storage_account_name" ] && [ -n "$storage_account_key" ] && [ -n "$fileshare_name" ]; then
+    update_fileshare_name
+    say 4 "Creating azure secret"
+    kubectl create secret generic bold-azure-secret --from-literal azurestorageaccountname=$storage_account_name --from-literal azurestorageaccountkey=$storage_account_key --type=Opaque
   else
-    say 3 "Skipping domain mapping as it is not provided"
+    say 3 "Skipping fileshare mounting details are not provided."
   fi
   
   say 4 "Deploying Bold BI application..."
@@ -181,7 +224,7 @@ function install_boldbi {
   show_bold_bi_graphic
 
   say 2 "Bold BI application deployed successfully!"
-  say 4 "You can access "boldbi" on your machine's IP with port number 30080, and Redis on port 32379."
+  say 4 "You can access "boldbi" on $app_base_url after mapping your machine ip with "$(echo "$app_base_url" | sed 's~^https\?://~~')""
 }
 
 install_boldbi
